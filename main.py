b01ec872ec2c056b2afd6e3f1879970f1031a565
Juan-789 <79994699+Juan-789@users.noreply.github.com>
2023-08-04 18:11:41 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+import turtle
+from turtle import *
+import random as ra
+
+
+tim = turtle.Turtle()
+turtle.colormode(255)
+
+
+def random_color():
+    r = ra.randint(0, 255)
+    g = ra.randint(0, 255)
+    b = ra.randint(0, 255)
+    return r, g, b
+
+
+directions = [0, 90, 180, 270]
+
+tim.speed('fastest')
+for i in range(4000):
+    tim.color(random_color())
+    tim.circle(100)
+    tim.right(17)
+
+
+Screen().exitonclick()
